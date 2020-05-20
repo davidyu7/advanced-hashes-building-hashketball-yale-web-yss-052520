f1a4110e7addd106eb21fdf7cf438eacd0fc43ab
David Yu <david.yu@yale.edu>
2020-05-20 18:52:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,18 @@
 def game_hash
   {
     home:{
-      
+      team_name: "Brooklyn Nets",
+      colors: ["Black, White"],
+      players: [
+        
+        ]
     },
     away:{
-      
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise, Purple"],
+      players: [
+        
+        ]
     }
   }
 end
